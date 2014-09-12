c3b8d1576861f7ecb0a1a4f2880090d683940b1c
tonyn <truongqnguien@gmail.com>
2014-09-11 22:40:31 -0600

    Adding threshold specification for smb_taskq_wait.d

--- smb-debug/dtrace/smb_taskq_wait.d
+++ smb-debug/dtrace/smb_taskq_wait.d
@@ -1,30 +1,34 @@
-#!/usr/sbin/dtrace -s
+#!/usr/sbin/dtrace -qs
 
 /*
  * smb_taskq_wait.d
  *
- * This script captures time requests spend waiting to be serviced, i.e.
+ * This script captures time requests spent waiting to be serviced, i.e.
  * waiting in taskq.
  *
  * Tony Nguyen <tony.nguyen@nexenta.com>
  *
  */
 
+BEGIN
+{
+        /* The default threshold is 10 msec */
+        delaytime = $1 > 0 ? $1 * 1000 : 10 * 1000;
+        printf("\nWarnings for tasks whose queue times > %d usecs\n", delaytime);
+}
+
 fbt::smb1sr_work:entry
 { self->tr = 1; self->sr = args[0]; }
 
-/* fbt::smb_com_nt_create_andx:entry */
 fbt::smb_com_*:entry
-/self->tr/
+/self->tr/ { self->func = probefunc; }
+
+fbt::smb1sr_work:return
+/self->tr && (self->sr->sr_time_active/1000 - self->sr->sr_time_submitted/1000) > delaytime/
 {
-        self->func = probefunc;
+        printf("%Y: %s %-10d\n", walltimestamp, self->func,
+            (self->sr->sr_time_active/1000 - self->sr->sr_time_submitted/1000));
 }
 
 fbt::smb1sr_work:return
-/self->tr/
-{
-        @[self->func]= quantize((self->sr->sr_time_active - self->sr->sr_time_submitted)/1000);
-        self->tr = 0; self->sr = 0; self->func = 0;
-}
-
-profile:::tick-5sec {printa(@); }
+/self->tr/ { self->tr = 0; self->sr = 0; self->func = 0; }
