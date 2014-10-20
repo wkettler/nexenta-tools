79d607f419c6e6c9eaf49296b737ec0a963cb6c4
William Kettler <william.p.kettler@gmail.com>
2014-10-20 15:19:11 -0500

    Major upgrades to start/stop script. Users can now start the scripts in the background and come back later and stop them.

--- stmf/stmf-debug.sh
+++ stmf/stmf-debug.sh
@@ -9,41 +9,117 @@
 #
 
 DATE=$(date +%Y-%m-%d:%H:%M:%S)
-DIR="logs/${DATE}"
-declare -a PIDS
+LOG="stmf-debug.log"
+PERF_DIR="perflogs/${DATE}"
+PID_LOG="/var/tmp/stmf-debug.pid"
 
-# Gracefully exit
-trap cleanup SIGINT
+ECHO="/usr/gnu/bin/echo -ne"
+KILL="/usr/bin/kill"
+RM="/usr/bin/rm"
+NOHUP="/usr/bin/nohup"
+MKDIR="/usr/bin/mkdir -p"
 
-cleanup() {
-    for p in "${PIDS[@]}"; do
-        kill "${p}"
-    done
-    exit
+usage() {
+    ${ECHO} "This is stmf-debug a performance gathering utility for "
+    ${ECHO} "NexentaStor.\n"
+    ${ECHO} "\n"
+    ${ECHO} "USAGE\n"
+    ${ECHO} "    $0 <command>\n"
+    ${ECHO} "COMMAND\n"
+    ${ECHO} "\n"
+    ${ECHO} "    start  : start collecting performance data.\n"
+    ${ECHO} "    status : displays any running dtrace scripts it has invoked.\n"
+    ${ECHO} "    stop   : attempt to stop the dtrace scripts it started.\n"
+    ${ECHO} "\n"
+}
+
+write_log() {
+    msg="`date +%Y-%m-%d:%H:%M:%S` [$1] $2\n"
+    ${ECHO} ${msg}
+    ${ECHO} ${msg} >> ${LOG}
 }
 
 background() {
-    $1 &
-    PIDS=("${PIDS[@]}" "$!")
+    write_log "INFO" "Running $1"
+    ${NOHUP} $1 2>&1 &
+    ${ECHO} "$!\n" >> ${PID_LOG}
 }
 
 background_log() {
-    $1 > "${DIR}/$2" &
-    PIDS=("${PIDS[@]}" "$!")
+    write_log "INFO" "Running $1"
+    ${NOHUP} $1 > "${PERF_DIR}/$2" 2>&1 &
+    ${ECHO} "$!\n" >> ${PID_LOG}
 }
 
-# Make log directory
-mkdir -p "${DIR}"
+stop() {
+    # Make sure the PID file exists o/w there isn't an active session
+    if [ -f ${PID_LOG} ]; then
+        write_log "INFO" "Stopping stmf-debug"
+        # Make sure each pid is alive
+        while read p; do
+            write_log "INFO" "Killing process ID ${p}"
+            ${KILL} "${p}"
+        done < ${PID_LOG}
 
-echo ""
-echo "Ctrl-C to stop monitoring..."
-echo ""
+        ${RM} ${PID_LOG}
+    else
+        write_log "ERROR" "stmf-debug is not running"
+    fi
+}
 
-background_log "dtrace/stmf_task_time_th.d 1000" "stmf_task_time_th.out"
-background_log "dtrace/stmf_worker_queue.sh 5" "stmf_worker_queue.out"
-background_log "dtrace/iscsit_sessions.d" "iscsit_session.out"
+start() {
+    # Make performance log directory
+    ${MKDIR} -p ${PERF_DIR}
 
-# Loop
-while true; do
-    read x
-done
+    if [ -f ${PID_LOG} ]; then
+        write_log "ERROR" "stmf-debug is already running"
+        return
+    fi
+
+    write_log "INFO" "Starting monitoring scripts"
+
+    # Define all collection scripts here
+    background_log "dtrace/stmf_task_time_th.d 1000" "stmf_task_time_th.out"
+    background_log "dtrace/stmf_worker_queue.sh 5" "stmf_worker_queue.out"
+    background_log "dtrace/iscsit_sessions.d" "iscsit_sessions.out"
+    
+    write_log "INFO" "Monitoring scripts started"
+}
+
+status() {
+    # Make sure the PID file exists o/w there isn't an active session
+    if [ -f ${PID_LOG} ]; then
+        write_log "INFO" "stmf-debug is running"
+        # Make sure each pid is alive
+        while read p; do
+            ${KILL} -0 "${p}" 2> /dev/null
+            if [ $? -eq 0 ]; then
+                write_log "INFO" "Process ID ${p} is running"
+            else
+                write_log "ERROR" "Process ID ${p} has died"
+            fi
+        done < ${PID_LOG}
+    else
+        write_log "INFO" "stmf-debug is not running"
+    fi
+}
+
+subcommand="$1"
+case "${subcommand}" in
+    start )
+        start
+        exit 0
+        ;;
+    stop )
+        stop
+        exit 0
+	    ;;
+    status )
+        status
+        exit 0
+	    ;;
+    * )
+        usage
+        exit 1
+	    ;;
+esac
