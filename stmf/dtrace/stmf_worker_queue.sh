51ab8ebf02450e8cc00c6c6d9529f69d80b6f862
William Kettler <william.p.kettler@gmail.com>
2014-11-17 10:37:23 -0600

    I am OCD.

--- stmf/dtrace/stmf_worker_queue.sh
+++ stmf/dtrace/stmf_worker_queue.sh
@@ -16,8 +16,7 @@ CUT="/usr/bin/cut"
 PRINTF="/usr/bin/printf"
 
 # Make sure only root can run script
-if [[ $EUID -ne 0 ]]
-then
+if [[ $EUID -ne 0 ]]; then
     ${ECHO} "This script must be run as root" 1>&2
     exit 1
 fi
