fca95bfc01ac31daef3cdb2df11a136cabcbe5d0
William Kettler <william.p.kettler@gmail.com>
2014-11-21 14:24:15 -0600

    Graceful exit and cleanup.

--- loop-iozone.sh
+++ loop-iozone.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
+#
+# Loop iozone for pool burn in.
+#
+
 pool=$1
 if [[ -z "$pool" ]] 
 then
-  echo "Please specify a pool name"
+  echo "Please specify a pool name."
   exit 1
 fi
 
-while true
-do
-echo Starting iozone loop on ${pool}
-date
-zfs create -o recordsize=32k -o compression=off ${pool}/iozone
-(cd /volumes/${pool}/iozone && iozone -ec -r 32 -s 1048576m -l 6 -i 0 -i 1 -i 8)
-zfs destroy ${pool}/iozone
-echo Waiting 30 seconds before starting again...
-sleep 30
+# Gracefully exit
+trap cleanup SIGINT
+
+cleanup() {
+    echo "User killed test."
+    destroy
+    exit
+}
+
+create() {
+    echo "Creating iozone test folder."
+    zfs create -o recordsize=32k -o compression=off ${pool}/iozone
+}
+
+destroy() {
+    echo "Destroying zfs folder."
+    zfs destroy ${pool}/iozone
+}
+
+create
+while true; do
+    echo "Starting iozone on ${pool}."
+    date
+    (cd /volumes/${pool}/iozone && iozone -ec -r 32 -s 1048576m -l 6 -i 0 -i 1 -i 8)
+    echo "Waiting 30 seconds before next iteration."
+    sleep 30
 done
