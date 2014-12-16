a44cf91fd24a2ab58b2ce7692700bfbca0c1363e
William Kettler <william.p.kettler@gmail.com>
2014-12-16 14:04:14 -0600

    Updated to commit changes on reboot. Will also now reboot remote system not local system script is run on.

--- dell-bios.sh
+++ dell-bios.sh
@@ -52,6 +52,55 @@ racadm_set() {
     fi
 }
 
+racadm_commit() {
+    #
+    # commit changes 
+    # see http://jonamiki.com/2014/10/18/racadm-change-bios-settings-create-commit-job-reboot-and-apply/
+    #
+    out=$(sshpass -p "$pass" ssh -o StrictHostKeyChecking=no "$host" "racadm jobqueue create BIOS.Setup.1-1" 2>&1)
+    # If sshpass fails best to exit immediately to avoid a hung iDRAC
+    if [ $? -ne 0 ]; then
+        echo "[FAILURE] sshpass"
+        echo "$out"
+        exit 1
+    fi
+
+    # We have no way of checking racadm return code so we must parse the output
+    # for an ERROR string
+    echo "$out" | grep -q ERROR
+    if [ $? -eq 0 ]; then
+        echo "[FAILURE] jobqueue create BIOS.Setup.1-1"
+        echo "$out"
+    else
+        echo "[SUCCESS] jobqueue create BIOS.Setup.1-1"
+    fi
+}
+
+racadm_reboot() {
+    #
+    # Issues a power-cycle operation on the managed server. This action is
+    # similar to pressing the power button on the system's front panel to power
+    # down and then power up the system.
+    #
+    out=$(sshpass -p "$pass" ssh -o StrictHostKeyChecking=no "$host" "racadm serveraction powercycle" 2>&1)
+    # If sshpass fails best to exit immediately to avoid a hung iDRAC
+    if [ $? -ne 0 ]; then
+        echo "[FAILURE] sshpass"
+        echo "$out"
+        exit 1
+    fi
+
+    # We have no way of checking racadm return code so we must parse the output
+    # for an ERROR string
+    echo "$out" | grep -q ERROR
+    if [ $? -eq 0 ]; then
+        echo "[FAILURE] serveraction powercycle"
+        echo "$out"
+    else
+        echo "[SUCCESS] serveraction powercycle"
+    fi
+}
+
 # Check command line parameters
 if [ $# -ne 1 ]; then
     echo Usage
@@ -95,8 +144,11 @@ racadm_set BIOS.SlotDisablement.Slot5 BootDriverDisabled
 racadm_set BIOS.SlotDisablement.Slot6 BootDriverDisabled
 racadm_set BIOS.SlotDisablement.Slot7 BootDriverDisabled
 
+# commit the change
+racadm_commit
+
 # Reboot
 echo "A reboot is required for the new settings to take effect."
 if prompt "Reboot now?"; then
-    reboot
+    racadm_reboot
 fi
