d262d7fea4829fbd2f52386fe7d41058b6123953
William Kettler <william.kettler@nexenta.com>
2015-10-20 13:24:34 -0500

    Uses the new smart disable all command in NMC

--- smart-disable.sh
+++ smart-disable.sh
@@ -4,25 +4,18 @@
 #
 # Disable SMART on all disks.
 #
-# Copyright (C) 2014  Nexenta Systems
+# Copyright (C) 2015  Nexenta Systems
 # William Kettler <william.kettler@nexenta.com>
 #
 
 echo "Disabling SMART collector..."
-nmc -c 'setup trigger nms-autosmartcheck disable'
 nmc -c 'setup collector smart-collector disable'
 
-echo "Discovering devices..."
-disks=$(nmc -c 'show lun slotmap' | grep c*t*d0 | cut -d" " -f1)
+echo "Disabling Auto SMART check..."
+nmc -c 'setup trigger nms-autosmartcheck disable'
 
-echo "Disabling SMART..."
-(
-for d in $disks; do
-    echo ""
-    echo "setup lun smart disable -d $d"
-    echo ""
-done
-) | nmc
+echo "Disabling SMART on all drives..."
+nmc -c 'setup lun smart disable all'
 
 # Review SMART status on each drive
 nmc -c 'show lun smartstat' | less
