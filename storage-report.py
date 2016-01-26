763d974f58628e2dab2b323d914efcee73382f12
William Kettler <william.kettler@nexenta.com>
2016-01-26 10:50:49 -0600

    Major updates including zfs list -o space and style

--- storage-report.py
+++ storage-report.py
@@ -7,57 +7,14 @@ Report storage utilization.
 
 Supported on NexentaStor 4.0.3 or greater.
 
-Copyright (c) 2014  Nexenta Systems
+Copyright (c) 2016  Nexenta Systems
 William Kettler <william.kettler@nexenta.com>
 """
 
 import sys
-import subprocess
-import re
 import time
-
-unit = 1024 ** 3
-unit_str = "G"
-
-
-def execute(cmd):
-    """
-    Execute a command in the default shell.
-
-    Inputs:
-        cmd     (str): Command to execute
-        timeout (int): Command timeout in seconds
-    Outputs:
-        output (list): STDOUT, STDERR is piped to STDOUT
-    """
-    try:
-        # Execute the command and wait for the subprocess to terminate
-        phandle = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
-                                   stderr=subprocess.STDOUT)
-        # Wait for the command to return
-        phandle.poll()
-
-        # Read the stdout/sterr buffers and retcode
-        stdout, stderr = phandle.communicate()
-        retcode = phandle.returncode
-    except Exception, e:
-        sys.stderr.write("[ERROR] unable to execute \"%s\"" % cmd)
-        sys.stderr.write(e)
-        sys.exit(1)
-
-    # Split lines into list
-    if stdout and stdout is not None:
-        output = stdout
-    else:
-        output = None
-
-    # Exit if command fails
-    if retcode:
-        sys.stderr.write("[ERROR] non-zero return code \"%s\"" % cmd)
-        sys.stderr.write(output)
-        sys.exit(1)
-
-    return output
+import commands
+import socket
 
 
 def pprint_table(table):
@@ -78,161 +35,25 @@ def pprint_table(table):
     # Print header
     header = table.pop(0)
     width = []
-    for i in range(len(header)):
-        width.append(len(header[i]))
-        col = header[i].rjust(padding[i] + 2)
+    for index, item in enumerate(header):
+        width.append(len(item))
+        col = item.rjust(padding[index] + 2)
         print col,
     print ""
 
     # Print header delimiter
-    for i in range(len(width)):
-        under = "-" * width[i]
-        col = under.rjust(padding[i] + 2)
+    for index, item in enumerate(width):
+        under = "-" * item
+        col = under.rjust(padding[index] + 2)
         print col,
     print ""
 
     # Print table contents
     for row in table:
-        for i in range(len(row)):
-            col = row[i].rjust(padding[i] + 2)
+        for index, item in enumerate(row):
+            col = item.rjust(padding[index] + 2)
             print col,
         print ""
-
-
-def get_zpools():
-    """
-    Return a list of zpools on the system excluding syspool.
-
-    Inputs:
-        None
-    Outputs:
-        zpools (list): zpools
-    """
-    zpools = []
-
-    cmd = "zpool list  -H -o name"
-    output = execute(cmd)
-
-    # Ignore syspool
-    for pool in output.splitlines():
-        if pool == 'syspool':
-            continue
-        else:
-            zpools.append(pool)
-
-    return zpools
-
-
-def get_usedsnap(zpool):
-    """
-    Get space used by all snapshots including direct descendents and snapshots
-    of child datasets and volumes.
-
-    Inputs:
-        zpool (str): zpool
-    Outputs:
-        usedsnap (str): Used snapshot space
-    """
-    cmd = "zfs list -rHp -o usedsnap  %s" % zpool
-    output = execute(cmd)
-
-    # Sum each line
-    usedsnap = "%.2f%s" % ((sum([float(l) for l in output.splitlines()]) /
-                            unit), unit_str)
-
-    return usedsnap
-
-
-def get_usedds(zpool):
-    """
-    Get spaced used by all datasets and volumes including direct descendents
-    and those of child datasets and volumes.
-
-    Inputs:
-        zpool (str): zpool
-    Output:
-        useds (int): Used dataset space
-    """
-    cmd = "zfs list -rHp -o usedds %s" % zpool
-    output = execute(cmd)
-
-    # Sum each line
-    usedds = "%.2f%s" % ((sum([float(l) for l in output.splitlines()]) / unit),
-                         unit_str)
-
-    return usedds
-
-
-def get_usedrefreserv(zpool):
-    """
-    Get space used by all refreservations including those reservations on all
-    direct and indirect descendents.
-
-    Inputs:
-        zpool (str): zpool
-    Ouputs:
-        usedrefreserv (int): Used refreservation space
-    """
-    cmd = "zfs list -rHp -o usedrefreserv %s" % zpool
-    output = execute(cmd)
-
-    # Sum each line
-    usedrefreserv = "%.2f%s" % ((sum([float(l) for l in output.splitlines()]) /
-                                 unit), unit_str)
-
-    return usedrefreserv
-
-
-def get_used(zpool):
-    """
-    Total space used which is the sum of all direct and indirect snapshot,
-    datasets, volumes, and reservations descendents.
-
-    Inputs:
-        zpool (str): zpool
-    Outputs:
-        used (int): used capacity
-    """
-    cmd = "zfs list -Hp -o used %s" % zpool
-    output = execute(cmd)
-
-    used = "%.2f%s" % ((float(output.strip()) / unit), unit_str)
-
-    return used
-
-
-def get_allocated(zpool):
-    """
-    Get space allocated, i.e actual space consumed on disk.
-
-    Inputs:
-        zpool (str): zpool
-    Outputs:
-        allocated (int): allocated space
-    """
-    cmd = "zpool list -Hp -o allocated %s" % zpool
-    output = execute(cmd)
-
-    allocated = "%.2f%s" % ((float(output.strip()) / unit), unit_str)
-
-    return allocated
-
-
-def get_size(zpool):
-    """
-    Get zpool size.
-
-    Inputs:
-        zpool (str): zpool
-    Outputs:
-        size (int): zpool size
-    """
-    cmd = "zpool list -Hp -o size %s" % zpool
-    output = execute(cmd)
-
-    size = "%.2f%s" % ((float(output.strip()) / unit), unit_str)
-
-    return size
 
 
 def get_hostname():
@@ -244,46 +65,77 @@ def get_hostname():
     Ouputs:
         hostname (str): hostname
     """
-    hostname = execute("hostname").strip()
+    hostname = socket.gethostname()
 
     return hostname
 
 
-def main():
-    date_str = time.strftime('%Y%m%d-%H%M', time.localtime(int(time.time())))
-    hostname = get_hostname()
-    ofile = "%s-%s-storage-report.csv" % (date_str, hostname)
+def get_zfs_space(unit):
+    """
+    Parse the output of `zfs list -o space` and convert to the defined unit.
+
+    Inputs:
+        unit (str): Unit, e.g. B,K,M,G,T
+    Outputs:
+        table (list): Pased output of `zfs list -o space`
+    """
+    units = {
+        "B": 1,
+        "K": 1024,
+        "M": 1024 ** 2,
+        "G": 1024 ** 3,
+        "T": 1024 ** 4
+    }
     table = []
+
+    retcode, output = commands.getstatusoutput("zfs list -p -o space")
+    if retcode:
+        print "[ERROR] Command execution failed"
+        print output
+        sys.exit(1)
+
+    # Iterate over the command output
+    header = True
+    for line in output.splitlines():
+        row = line.split()
+        if header:
+            header = False
+        else:
+            for i in range(1, len(row)):
+                row[i] = "%.1f%s" % (float(int(row[i]) / units[unit]), unit)
+        table.append(row)
+
+    return table
+
+
+def main():
+    """
+    Main function.
+    """
+    unit = "K"
+    date = time.strftime('%Y%m%d-%H%M', time.localtime(int(time.time())))
+    hostname = get_hostname()
+    ofile = "%s-%s-storage-report.csv" % (date, hostname)
 
     # Open output file
     try:
-        fh = open(ofile, 'w')
-    except Exception, e:
+        fhandle = open(ofile, 'w')
+    except IOError, err:
         sys.stderr.write("[ERROR] opening %s" % ofile)
-        sys.stderr.write(e)
+        sys.stderr.write(err)
         sys.exit(1)
 
-    # Header
-    header = ["Pool", "Size", "Allocated", "Used", "Used Snapshot",
-              "Used Data", "Used RefReservation"]
-    fh.write("%s\n" % ",".join(header))
-    table.append(header)
-
-    # Iterate over each pool
-    for z in get_zpools():
-        row = [z, get_size(z), get_allocated(z), get_used(z), get_usedsnap(z),
-               get_usedds(z), get_usedrefreserv(z)]
-
-        fh.write("%s\n" % ",".join([str(x) for x in row]))
-        table.append(row)
+    space = get_zfs_space(unit)
+    for row in space:
+        fhandle.write("%s\n" % ",".join(row))
 
     # Close file
-    fh.close()
+    fhandle.close()
 
-    pprint_table(table)
+    pprint_table(space)
 
     print ""
-    print "Output save to %s." % ofile
+    print "Output saved to %s." % ofile
 
 
 if __name__ == "__main__":
